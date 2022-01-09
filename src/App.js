2acf30b4c8429b3986824ede9879df1540412300
ajholley24 <ajholley.24@gmail.com>
2022-01-08 23:58:38 -0600

    updated react web app page commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import greencheck from "./greencheck.png";
+
+function Header(props){
+  
+  return(
+    <header>
+      <h1>{props.name}</h1>
+    </header>
+  );
+}
+
+function Main(props){
+  return(
+    <section>
+      <p>Born {props.adjective} June 01, 1999</p>
+      <img src="https://github.com/ajholley24.png" height={200}/>
+      <ul style = {{textAlign: "left" }}>
+        {props.skills.map((skill) => (
+          <li key = {skill.id}>{skill.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props){
+  return(
+    <section>
+      <p>Website created {props.year} </p>
+    </section>
+  );
+}
+
+const skills = [
+  "C + Python Programming",
+  "Embedded Programming",
+  "Canbus Communication Networks"
+];
+
+const skillObjects = skills.map((skill, i) => ({id: i, title: skill}));
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name = "Aaron Jacob Holley"/>
+      <Main adjective = "on" skills = {skillObjects} />
+      <Footer year = {new Date().getFullYear()} />
     </div>
   );
 }
